b4ea7aa6aec99ddaefb5756c6fcfb2bc030d7cd1
Rycolopot <andriakyso@gmail.com>
2022-06-24 22:41:45 +0700

    node-testnet
    @paperhang

---
+++ killerquen.sh
@@ -0,0 +1,26 @@
+
+
+echo -e "\e[1m\e[32m"
+echo " 📌 Jangan Lupa di follow biar semangat bantu kalian 😅"
+echo " 📌 Twitter  : @rehan_ssf"
+echo " 📌 Telegram : @paperhang"
+echo -e "\e[0m"
+echo " seperti biasa di update dulu ya bang biar gak eror nanti "
+echo -n " klik enter aja bang 😂 !"
+read user
+echo
+echo
+
+
+echo -e "\e[1m\e[32m3. Downloading and building binaries... \e[0m" && sleep 1
+# download binary
+cd $HOME
+rm quicksilver -rf
+git clone https://github.com/ingenuity-build/quicksilver.git --branch v0.4.0
+cd quicksilver
+make build
+sudo chmod +x ./build/quicksilverd && sudo mv ./build/quicksilverd /usr/local/bin/quicksilverd
+
+sudo systemctl daemon-reload
+sudo systemctl enable quicksilverd
+sudo systemctl restart quicksilverd && sudo journalctl -u quicksilverd -f -o cat
