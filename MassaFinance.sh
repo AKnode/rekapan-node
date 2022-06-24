cea8b91eab33c9d789a2f685ae88f5ebecd99f23
Rycolopot <andriakyso@gmail.com>
2022-06-24 16:33:05 +0700

    node-tesnet
    @paperhang

---
+++ MassaFinance.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+echo -e "\e[1m\e[32m"
+echo " 📌 Jangan Lupa di follow biar semangat bantu kalian 😅"
+echo " 📌 Twitter  : @rehan_ssf"
+echo " 📌 Telegram : @paperhang"
+echo -e "\e[0m"
+sleep 15
---
+++ seid.sh
@@ -0,0 +1,147 @@
+
+#!/bin/sh
+
+
+echo -e "\e[1m\e[32m"
+echo " 📌 Jangan Lupa di follow biar semangat bantu kalian 😅"
+echo " 📌 Twitter  : @rehan_ssf"
+echo " 📌 Telegram : @paperhang"
+echo -e "\e[0m"
+echo " seperti biasa di update dulu ya bang biar gak eror nanti "
+echo -n " klik enter aja bang 😂 !"
+read user
+echo
+echo -e "\e[1m\e[32m1. Update... \e[0m" && sleep 1
+echo
+sudo apt update && sudo apt upgrade -y
+
+echo -e "\e[1m\e[32m2. Install pendukung.. \e[0m" && sleep 1
+echo
+sudo apt install curl build-essential git wget jq make gcc tmux -y
+echo
+
+echo -e "\e[1m\e[32m4. Membuat Moniker... \e[0m" && sleep 1
+echo
+# set vars
+if [ ! $NODENAME ]; then
+	read -p "masukan nama nodemu: " NODENAME
+	echo 'export NODENAME='$NODENAME >> $HOME/.bash_profile
+fi
+SEI_PORT=12
+if [ ! $WALLET ]; then
+	echo "export WALLET=wallet" >> $HOME/.bash_profile
+fi
+echo "export SEI_CHAIN_ID=sei-testnet-2" >> $HOME/.bash_profile
+echo "export SEI_PORT=${SEI_PORT}" >> $HOME/.bash_profile
+source $HOME/.bash_profile
+echo
+echo
+echo '🌀 --------------------------'
+echo -e "nama node mu: \e[1m\e[32m$NODENAME\e[0m"
+echo -e "nama wallet mu: \e[1m\e[32m$WALLET\e[0m"
+echo -e "chain-id: \e[1m\e[32m$SEI_CHAIN_ID\e[0m"
+echo -e "port: \e[1m\e[32m$SEI_PORT\e[0m"
+echo '----------------------------🌀'
+sleep 2
+echo
+echo
+# install go
+ver="1.18.2"
+cd $HOME
+wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz"
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz"
+rm "go$ver.linux-amd64.tar.gz"
+echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> ~/.bash_profile
+source ~/.bash_profile
+go version
+echo
+echo
+echo -e "\e[1m\e[32m3. Download binaries... \e[0m" && sleep 1
+cd $HOME && rm $HOME/sei-chain -rf
+git clone https://github.com/sei-protocol/sei-chain.git && cd $HOME/sei-chain
+git checkout 1.0.3beta
+make install
+mv ~/go/bin/seid /usr/local/bin/seid
+echo
+echo
+# config
+seid config chain-id $SEI_CHAIN_ID
+seid config keyring-backend test
+seid config node tcp://localhost:${SEI_PORT}657
+
+# init
+seid init $NODENAME --chain-id $SEI_CHAIN_ID
+
+# download genesis and addrbook
+wget -qO $HOME/.sei/config/genesis.json "https://raw.githubusercontent.com/sei-protocol/testnet/master/sei-testnet-2/genesis.json"
+wget -qO $HOME/.sei/config/addrbook.json "https://raw.githubusercontent.com/sei-protocol/testnet/master/sei-testnet-2/addrbook.json"
+echo
+# set peers and seeds
+SEEDS=""
+PEERS="585727dac5df8f8662a8ff42052a9584a1f7ee95@165.22.25.77:26656,2f047e234cb8b99fe8b9fee0059a5bc45042bc97@95.216.84.188:26656,bab4849cf3918c37b04cd3714984d1765616a4b2@49.12.76.255:36656,ab955dc7f70d8613ab4b554868d4658fd70b797b@217.79.180.194:26656,39c4bcaded0d1d886f2788ae955f1939406f3e7d@65.108.198.54:26696,9db58dba3b6354177fb428caccf5167c616ad4a1@167.235.28.18:26656,2f2804434afda302c86eb89eca27503e49a8a260@65.21.131.215:26696,38b4d78c7d6582fb170f6c19330a7e37e6964212@194.163.189.114:46656,7c5ee0d66a15013f0a771055378c5316331f17ba@95.216.101.84:25646,6f71bcbe347069fc4df9b607f6b843226e8deb71@95.217.221.201:26656"
+sed -i -e "s/^seeds *=.*/seeds = \"$SEEDS\"/; s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $HOME/.sei/config/config.toml
+
+# set custom ports
+sed -i.bak -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:${SEI_PORT}658\"%; s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://127.0.0.1:${SEI_PORT}657\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:${SEI_PORT}060\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:${SEI_PORT}656\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":${SEI_PORT}660\"%" $HOME/.sei/config/config.toml
+sed -i.bak -e "s%^address = \"tcp://0.0.0.0:1317\"%address = \"tcp://0.0.0.0:${SEI_PORT}317\"%; s%^address = \":8080\"%address = \":${SEI_PORT}080\"%; s%^address = \"0.0.0.0:9090\"%address = \"0.0.0.0:${SEI_PORT}090\"%; s%^address = \"0.0.0.0:9091\"%address = \"0.0.0.0:${SEI_PORT}091\"%" $HOME/.sei/config/app.toml
+
+# disable indexing
+indexer="null"
+sed -i -e "s/^indexer *=.*/indexer = \"$indexer\"/" $HOME/.sei/config/config.toml
+
+# config pruning
+pruning="custom"
+pruning_keep_recent="100"
+pruning_keep_every="0"
+pruning_interval="50"
+sed -i -e "s/^pruning *=.*/pruning = \"$pruning\"/" $HOME/.sei/config/app.toml
+sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"$pruning_keep_recent\"/" $HOME/.sei/config/app.toml
+sed -i -e "s/^pruning-keep-every *=.*/pruning-keep-every = \"$pruning_keep_every\"/" $HOME/.sei/config/app.toml
+sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"$pruning_interval\"/" $HOME/.sei/config/app.toml
+
+# set minimum gas price
+sed -i -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0usei\"/" $HOME/.sei/config/app.toml
+
+# enable prometheus
+sed -i -e "s/prometheus = false/prometheus = true/" $HOME/.sei/config/config.toml
+
+# reset
+seid tendermint unsafe-reset-all
+echo
+echo -e "\e[1m\e[32m4. Starting service... \e[0m" && sleep 1
+echo
+echo
+# create service
+tee $HOME/seid.service > /dev/null <<EOF
+[Unit]
+Description=seid
+After=network.target
+[Service]
+Type=simple
+User=$USER
+ExecStart=$(which seid) start
+Restart=on-failure
+RestartSec=10
+LimitNOFILE=65535
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo mv $HOME/seid.service /etc/systemd/system/
+
+# start service
+sudo systemctl daemon-reload
+sudo systemctl enable seid
+sudo systemctl restart seid
+echo
+echo
+echo
+echo 'INSTALASI SELESAI  🚀 '
+echo
+echo -ne "${lightgreen}\e[1m\e[32mToday is:\t\t\e[0m${red}" `date`; echo ""
+echo -e "${lightgreen}\e[1m\e[32mKernel Information: \t\e[0m${red}" "Linux 5.10.0-BSA_OS-amd64 x86_64"
+echo -e 'untuk mengecek logs: \e[1m\e[32mjournalctl -u seid -f -o cat\e[0m'
+echo -e "untuk mengecek status sync: \e[1m\e[32mseid status 2>&1 | jq .SyncInfo\e[0m"
+echo
+export PS1="\[\033[1;33m\]\u\[\033[1;37m\]@\[\033[1;32m\]\h\[\033[1;37m\]:\[\033[1;31m\]\w \[\033[1;36m\]\\$ \[\033[0m\]";
