694e9da17282ca604bb21acecf1f95fd8a4fbb6d
Rycolopot <andriakyso@gmail.com>
2022-06-25 01:12:24 +0700

    node-testnet
    @paperhang

---
+++ Deweb/deweb.sh
@@ -0,0 +1,137 @@
+#!/bin/sh
+
+
+echo -e "\e[1m\e[32m"
+echo " 📌 Jangan Lupa di follow biar semangat bantu kalian 😅"
+echo " 📌 Twitter  : @rehan_ssf"
+echo " 📌 Telegram : @paperhang"
+echo -e "\e[0m"
+echo " seperti biasa di update dulu ya bang biar gak eror nanti "
+echo -n " klik enter aja bang 😂 !"
+read user
+echo
+echo -e "\e[1m\e[32m4. Membuat Moniker... \e[0m" && sleep 1
+echo
+# set vars
+if [ ! $NODENAME ]; then
+	read -p "\e[1m\e[32mNama node : \e[0m" NODENAME
+	echo 'export NODENAME='$NODENAME >> $HOME/.bash_profile
+fi
+DEWEB_PORT=14
+if [ ! $WALLET ]; then
+	echo "export WALLET=wallet" >> $HOME/.bash_profile
+fi
+echo "export DEWEB_CHAIN_ID=deweb-testnet-1" >> $HOME/.bash_profile
+echo "export DEWEB_PORT=${DEWEB_PORT}" >> $HOME/.bash_profile
+source $HOME/.bash_profile
+
+echo '🌀 --------------------------'
+echo -e "nama node mu: \e[1m\e[32m$NODENAME\e[0m"
+echo -e "nama wallet mu: \e[1m\e[32m$WALLET\e[0m"
+echo -e "chain name: \e[1m\e[32m$DEWEB_CHAIN_ID\e[0m"
+echo -e "port: \e[1m\e[32m$DEWEB_PORT\e[0m"
+echo '----------------------------🌀'
+sleep 2
+
+echo -e "\e[1m\e[32m1. Update... \e[0m" && sleep 1
+# update
+sudo apt update && sudo apt upgrade -y
+
+echo -e "\e[1m\e[32m2. Install pendukung.. \e[0m" && sleep 1
+# packages
+sudo apt install curl build-essential git wget jq make gcc tmux -y
+
+# install go
+ver="1.18.2"
+cd $HOME
+wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz"
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz"
+rm "go$ver.linux-amd64.tar.gz"
+echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> ~/.bash_profile
+source ~/.bash_profile
+go version
+
+echo -e "\e[1m\e[32m3. build binaries... \e[0m" && sleep 1
+# download binary
+cd $HOME
+git clone https://github.com/deweb-services/deweb.git
+cd deweb
+git checkout v0.2
+make build
+sudo cp build/dewebd /usr/local/bin/dewebd
+
+# config
+dewebd config chain-id $DEWEB_CHAIN_ID
+dewebd config keyring-backend test
+dewebd config node tcp://localhost:${DEWEB_PORT}657
+
+# init
+dewebd init $NODENAME --chain-id $DEWEB_CHAIN_ID
+
+# download genesis and addrbook
+wget -qO $HOME/.deweb/config/genesis.json "https://raw.githubusercontent.com/deweb-services/deweb/main/genesis.json"
+wget -qO $HOME/.deweb/config/addrbook.json "https://raw.githubusercontent.com/kj89/testnet_manuals/main/dws/addrbook.json"
+
+# set peers and seeds
+SEEDS="74d8f92c37ffe4c6393b3718ca531da8f0bf0594@seed1.deweb.services:26656"
+PEERS="316d8e681019f25b078b36a26f349e48c916aace@161.97.104.113:26656,4172ea44cb18d7b8040c3c284d76340e9212fea7@95.214.53.225:26666,d2e9b0a8c0a07422c01e2477c8054ec7f2b509b6@128.199.49.193:26656,cba40403295977211209fb3b3037501e6f00f339@167.235.243.214:26656"
+sed -i -e "s/^seeds *=.*/seeds = \"$SEEDS\"/; s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $HOME/.deweb/config/config.toml
+
+# set custom ports
+sed -i.bak -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:${DEWEB_PORT}658\"%; s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://127.0.0.1:${DEWEB_PORT}657\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:${DEWEB_PORT}060\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:${DEWEB_PORT}656\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":${DEWEB_PORT}660\"%" $HOME/.deweb/config/config.toml
+sed -i.bak -e "s%^address = \"tcp://0.0.0.0:1317\"%address = \"tcp://0.0.0.0:${DEWEB_PORT}317\"%; s%^address = \":8080\"%address = \":${DEWEB_PORT}080\"%; s%^address = \"0.0.0.0:9090\"%address = \"0.0.0.0:${DEWEB_PORT}090\"%; s%^address = \"0.0.0.0:9091\"%address = \"0.0.0.0:${DEWEB_PORT}091\"%" $HOME/.deweb/config/app.toml
+
+# disable indexing
+indexer="null"
+sed -i -e "s/^indexer *=.*/indexer = \"$indexer\"/" $HOME/.deweb/config/config.toml
+
+# config pruning
+pruning="custom"
+pruning_keep_recent="100"
+pruning_keep_every="0"
+pruning_interval="50"
+sed -i -e "s/^pruning *=.*/pruning = \"$pruning\"/" $HOME/.deweb/config/app.toml
+sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"$pruning_keep_recent\"/" $HOME/.deweb/config/app.toml
+sed -i -e "s/^pruning-keep-every *=.*/pruning-keep-every = \"$pruning_keep_every\"/" $HOME/.deweb/config/app.toml
+sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"$pruning_interval\"/" $HOME/.deweb/config/app.toml
+
+# set minimum gas price
+sed -i -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0udws\"/" $HOME/.deweb/config/app.toml
+
+# enable prometheus
+sed -i -e "s/prometheus = false/prometheus = true/" $HOME/.deweb/config/config.toml
+
+# reset
+dewebd unsafe-reset-all
+
+echo -e "\e[1m\e[32m4. Starting service... \e[0m" && sleep 1
+# create service
+sudo tee /etc/systemd/system/dewebd.service > /dev/null <<EOF
+[Unit]
+Description=deweb
+After=network-online.target
+
+[Service]
+User=$USER
+ExecStart=$(which dewebd) start
+Restart=on-failure
+RestartSec=3
+LimitNOFILE=65535
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# start service
+sudo systemctl daemon-reload
+sudo systemctl enable dewebd
+sudo systemctl restart dewebd
+echo
+echo
+echo
+echo 'INSTALASI SELESAI  🚀 '
+echo
+echo -ne "${lightgreen}\e[1m\e[32mToday is:\t\t\e[0m${red}" `date`; echo ""
+echo -e "${lightgreen}\e[1m\e[32mKernel Information: \t\e[0m${red}" "Linux 5.10.0-BSA_OS-amd64 x86_64"
+echo -e 'To check logs: \e[1m\e[32mjournalctl -u dewebd -f -o cat\e[0m'
