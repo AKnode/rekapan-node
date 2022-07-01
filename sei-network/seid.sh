e9835de31e7d8bde95d3d5680164d6967e736de9
Rycolopot <andriakyso@gmail.com>
2022-07-02 04:26:04 +0700

    Update seid.sh

--- sei-network/seid.sh
+++ sei-network/seid.sh
@@ -57,51 +57,64 @@ wget https://go.dev/dl/go1.18.1.linux-amd64.tar.gz \
 
 rm go1.18.1.linux-amd64.tar.gz
 
-git clone https://github.com/sei-protocol/sei-chain.git \
-&& cd sei-chain && git checkout 1.0.4beta && make install \
-&& seid version
-wget -O $HOME/.sei/config/genesis.json https://raw.githubusercontent.com/sei-protocol/testnet/master/sei-testnet-2/genesis.json 
-echo "[Unit]
+cd || return
+rm -rf sei-chain
+git clone https://github.com/sei-protocol/sei-chain.git
+cd sei-chain || return
+git checkout 1.0.5beta
+make install
+seid version # 1.0.5beta
 
-Description=Seid Node
-After=network.target
-#
+# replace nodejumper with your own moniker, if you'd like
+seid config chain-id sei-testnet-2
+seid init nodejumper --chain-id sei-testnet-2 -o
+
+curl https://raw.githubusercontent.com/sei-protocol/testnet/master/sei-testnet-2/genesis.json > ~/.sei/config/genesis.json
+sha256sum $HOME/.sei/config/genesis.json # aec481191276a4c5ada2c3b86ac6c8aad0cea5c4aa6440314470a2217520e2cc
+
+curl https://raw.githubusercontent.com/sei-protocol/testnet/master/sei-testnet-2/addrbook.json > ~/.sei/config/addrbook.json
+sha256sum $HOME/.sei/config/addrbook.json # 9058b83fca36c2c09fb2b7c04293382084df0960b4565090c21b65188816ffa6
+
+sed -i 's|^minimum-gas-prices *=.*|minimum-gas-prices = "0.0001usei"|g' $HOME/.sei/config/app.toml
+seeds=""
+peers="f4b1aa3416073a4493de7889505fc19777326825@rpc1-testnet.nodejumper.io:28656"
+sed -i -e 's|^seeds *=.*|seeds = "'$seeds'"|; s|^persistent_peers *=.*|persistent_peers = "'$peers'"|' $HOME/.sei/config/config.toml
+
+# in case of pruning
+sed -i 's|pruning = "default"|pruning = "custom"|g' $HOME/.sei/config/app.toml
+sed -i 's|pruning-keep-recent = "0"|pruning-keep-recent = "100"|g' $HOME/.sei/config/app.toml
+sed -i 's|pruning-interval = "0"|pruning-interval = "10"|g' $HOME/.sei/config/app.toml
+
+sudo tee /etc/systemd/system/seid.service > /dev/null << EOF
+[Unit]
+Description=Sei Protocol Node
+After=network-online.target
 [Service]
 User=$USER
-Type=simple
 ExecStart=$(which seid) start
 Restart=on-failure
-LimitNOFILE=65535
-#
+RestartSec=10
+LimitNOFILE=10000
 [Install]
-WantedBy=multi-user.target" > seid.service \
-&& sudo cp seid.service /etc/systemd/system
+WantedBy=multi-user.target
+EOF
 
-seid tendermint unsafe-reset-all --home $HOME/.sei
+seid tendermint unsafe-reset-all --home $HOME/.sei --keep-addr-book
 
-SNAP_RPC1="http://173.212.215.104:26357" \
-SNAP_RPC2="http://173.212.215.104:26357"
-
-LATEST_HEIGHT=$(curl -s $SNAP_RPC2/block | jq -r .result.block.header.height); \
-BLOCK_HEIGHT=$((LATEST_HEIGHT - 700)); \
-TRUST_HASH=$(curl -s "$SNAP_RPC2/block?height=$BLOCK_HEIGHT" | jq -r .result.block_id.hash)
+SNAP_RPC="http://rpc1-testnet.nodejumper.io:28657"
+LATEST_HEIGHT=$(curl -s $SNAP_RPC/block | jq -r .result.block.header.height); \
+BLOCK_HEIGHT=$((LATEST_HEIGHT - 2000)); \
+TRUST_HASH=$(curl -s "$SNAP_RPC/block?height=$BLOCK_HEIGHT" | jq -r .result.block_id.hash)
 
 echo $LATEST_HEIGHT $BLOCK_HEIGHT $TRUST_HASH
 
-sed -i.bak -E "s|^(enable[[:space:]]+=[[:space:]]+).*$|\1true| ; \
-s|^(rpc_servers[[:space:]]+=[[:space:]]+).*$|\1\"$SNAP_RPC1,$SNAP_RPC2\"| ; \
+sed -i -E "s|^(enable[[:space:]]+=[[:space:]]+).*$|\1true| ; \
+s|^(rpc_servers[[:space:]]+=[[:space:]]+).*$|\1\"$SNAP_RPC,$SNAP_RPC\"| ; \
 s|^(trust_height[[:space:]]+=[[:space:]]+).*$|\1$BLOCK_HEIGHT| ; \
 s|^(trust_hash[[:space:]]+=[[:space:]]+).*$|\1\"$TRUST_HASH\"|" $HOME/.sei/config/config.toml
 
-peers="f6c80c797ab4b3161fbf758ed23573c11ea5d559@173.212.215.104:26356"
-sed -i.bak -e "s/^persistent_peers *=.*/persistent_peers = \"$peers\"/" $HOME/.sei/config/config.toml
-
-sudo tee <<EOF >/dev/null /etc/systemd/journald.conf
-Storage=persistent
-EOF
-
-sudo systemctl enable seid.service
 sudo systemctl daemon-reload
+sudo systemctl enable seid
 sudo systemctl restart seid
 
 sudo systemctl stop seid.service
